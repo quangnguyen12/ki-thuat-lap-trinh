95ceb79f95c0eaefb1ce1b0f7865629d699f28af
quangnguyen12 <nguyenbaoquang1@gmail.com>
2022-04-05 20:28:12 +0700

    them sign in form sql

---
+++ test/test/Controllers/SignInController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test.Models;
+
+
+namespace test.Controllers
+{
+    public class SignInController : Controller
+    {
+         DBusersignuploginEntities db = new DBusersignuploginEntities();    
+
+        // GET: SignIn
+        public ActionResult Dbuserinfo()
+        {
+            return View(db.tblUserInfoes.ToList());
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(tblUserInfo tblUserinfo)
+        {
+            if (db.tblUserInfoes.Any(x=>x.usernameus==tblUserinfo.usernameus))
+            {
+                ViewBag.Notification = "Tai khoan nafy da duoc dang ki boi nguoi dung truoc";
+                return View();
+            }
+            else
+            {
+                db.tblUserInfoes.Add(tblUserinfo);
+                db.SaveChanges();
+                Session ["IdUsSs"]= tblUserinfo.iduser.ToString();
+                Session ["UsernameSs"] = tblUserinfo.usernameus.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+
+    }
+}
